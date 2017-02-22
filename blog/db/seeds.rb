d82544ff9d633332bd4932c0db3dc13944d9d32a
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-02-22 13:16:39 -0500

    made seed, need to check behaviour in irb

--- blog/db/seeds.rb
+++ blog/db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+5.times do |t|
+  User.new.tap do |user|
+    user.first_name = Faker::Name.first_name
+    user.last_name = Faker::Name.last_name
+    user.email = Faker::Internet.email
+    user.password = '12345678'
+    user.save
+  end
+end
+
+users = User.all
+
+users.each do |user|
+  3.times do
+    user.posts.create(title: Faker::Lorem.sentence,
+                      body: Faker::Lorem.paragraph)
+  end
+end
