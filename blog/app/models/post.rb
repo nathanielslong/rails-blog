c8d702fb90ac1c9b77d7098c75b90e5a3720d995
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-02-22 12:58:39 -0500

    trying to add user-pposts relation

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :user
   # Checks that title is not longer than 150 char
   validates :title, presence: true, length: {maximum: 150}
   # Checks that a post body exists
--- blog/db/migrate/20170222165348_create_posts.rb
+++ blog/db/migrate/20170222165348_create_posts.rb
@@ -1,6 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
+      t.belongs_to :user, index: true
       t.string :title
       t.text :body
 
