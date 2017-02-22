837416dd73e305afbd93f791579ccbf20ef41542
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-02-22 15:58:51 -0500

    adding the has-many to the user and posts made activerecord make more sense

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+
   has_many :comments
   # Checks that title is not longer than 150 char
   validates :title, presence: true, length: {maximum: 150}
