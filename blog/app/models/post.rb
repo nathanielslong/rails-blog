f85d3e7ed57ee24d2c27d69a0cc3264c609faa7d
Nathaniel Long <nathaniel.s.long@gmail.com>
2017-02-22 12:42:55 -0500

    allowed actions on index

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  # Checks that title is not longer than 150 char
+  validates :title, presence: true, length: {maximum: 150}
+  # Checks that a post body exists
+  validates :body, presence: true
 end
